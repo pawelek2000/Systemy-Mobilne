698b7b7624b9fa62ed8d27486c9952d4ab5beff9
pawelek2000 <65534106+pawelek2000@users.noreply.github.com>
2021-11-29 21:42:00 +0100

    Add files via upload

---
+++ Zadanie6/app/src/main/java/com/example/zadanie6/TaskStorage.java
@@ -0,0 +1,37 @@
+package com.example.zadanie6;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class TaskStorage {
+    private static final TaskStorage taskStorage = new TaskStorage();
+    private final List<Task> tasks = new ArrayList<>();
+    public static TaskStorage getInstance() {
+        return taskStorage;
+    }
+
+    public Task getTask(UUID id) {
+        for (Task task : tasks) {
+            if (task.getId().equals(id)) {
+                return task;
+            }
+        }
+        return null;
+    }
+
+
+    private TaskStorage() {
+        for (int i = 1; i < 101; i++) {
+            Task task = new Task();
+            task.setName("Zadanie #" + i);
+            task.setDone(i % 3 == 0);
+            tasks.add(task);
+        }
+    }
+
+    public List<Task> getTasks() {
+        return  tasks;
+    }
+}
